681b7a25f5cbd9bf66c0385747faa80a0c0a9a9b
Newton Bii <biinewton382@gmail.com>
2018-02-08 14:32:38 +0300

    install bootstrap and load it in the templates.

--- faith_pms/models.py
+++ faith_pms/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class Patient(models.Model):
     name = models.CharField(max_length=100)
     date_of_birth = models.DateTimeField()
-    phone_number = models.IntegerField(default=0700000000)
+    phone_number = models.IntegerField(default=0)
     gender = models.CharField(max_length=6)
-    id_number = models.IntegerField(default=00000000)
+    id_number = models.IntegerField(default=0)
     status = models.CharField(max_length=8)
     email = models.EmailField()
-    location = models.
+    nhif_number = models.IntegerField(default=0)
+    blood_group = models.CharField(max_length=5)
--- pms/settings.py
+++ pms/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'faith_pms.apps.FaithPmsConfig'
 ]
 
 MIDDLEWARE = [
