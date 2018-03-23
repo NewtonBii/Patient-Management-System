4d6bd4d56433c476dd1caf6a6a7c07e91c6e6faa
Newton Bii <biinewton382@gmail.com>
2018-03-23 14:33:59 +0300

    Create serializer module to create api.

---
+++ faith_pms/serializer.py
@@ -0,0 +1,11 @@
+# import serializers module from the rest_framework package
+from rest_framework import serializers
+
+# import UserInformation class from the models module
+from .models import Doctor
+
+# create a UserInformationSerializer class that will inhertit form the ModelSerializer class
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('title', 'description', 'name', 'phone_number', 'license_number', 'specialty', 'hospital', 'profile_photo', 'user', 'email')
